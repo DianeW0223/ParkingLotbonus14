58d548943a0b17267d0f60f42b57708a26d82ead
DianeW0223 <dwatts@motor.com>
2017-07-10 20:25:05 -0400

    initial commit

---
+++ Garage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingBonus13
+{
+    class Garage
+    {
+        private int level;
+        private int vehicles;
+        //private int motorcycle;
+        private int bus;
+        
+
+        public Garage(int level , int vehicles)
+        {
+            this.vehicles = vehicles;
+            this.level = level;
+            
+            
+        }
+
+                
+        public int getBus(int x)
+        {
+            bus = 5;
+            return bus;
+        }
+
+        public int getSpaces(int x, int y)
+        {
+            int Spaces = x * vehicles * y;
+            return Spaces;
+        }
+
+        public int getBusSpace(int x, int y)
+        {
+            int BusSpace = x / getBus(bus)*y;
+            return BusSpace;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingBonus13
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Gathering information from the user.
+            bool run = true;
+            
+            Console.WriteLine("Welcome to the parking garage app");
+            while (run == true)
+            {
+                Console.WriteLine("How many levels will your garage have?");
+                int level = int.Parse(Console.ReadLine());
+                Console.WriteLine("How many cars and motorcycles do you want to park on each level?");
+                int vehicles = int.Parse(Console.ReadLine());
+
+                //using the garage object to get the counts to display the user.
+                Garage myGarage = new Garage(level, vehicles);
+                int carSpace = myGarage.getSpaces(vehicles, level);
+                int busSpace = myGarage.getBusSpace(vehicles, level);
+
+                //Information displaying for the user to see.
+                Console.WriteLine("Your parking garage can hold up to " + carSpace + " cars and/or motorcycles");
+                Console.WriteLine("Your parking garage could possible hold up to " + busSpace + " buses, depending on the number of cars and vehicles in the garage");
+                
+                run = Continue();
+            }
+            
+        }
+        public static Boolean Continue()
+        {
+            Boolean run;
+            
+            Console.WriteLine("Would you like to build another garage? Y/N");
+            String answer = Console.ReadLine();
+            
+
+            if ((answer == "Y") || (answer == "y"))
+            {
+                run = true;
+                
+
+            }
+            else if ((answer == "N") || (answer == "n"))
+            {
+                run = false;
+
+                Console.WriteLine("Good Bye!");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry I don't understand. Let's try again");
+                run = Continue();
+            }
+            return run;
+        }
+    }
+}
